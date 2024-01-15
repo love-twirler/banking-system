9c8070c1bbe2c4ca47bb9f77e6c6b2476b3728ef
Lu <152173746+love-twirler@users.noreply.github.com>
2024-01-15 13:55:47 +0600

    Create Customer.h

---
+++ Customer.h
@@ -0,0 +1,24 @@
+// Customer.h
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+#include <vector>
+#include "Account.h"
+
+class Account; // Forward declaration
+
+class Customer {
+public:
+    Customer(int customerId, const std::string& name, const std::string& email);
+    void addAccount(Account* account);
+    void displayCustomerInfo();
+
+private:
+    int customerId;
+    std::string name;
+    std::string email;
+    std::vector<Account*> accounts;
+};
+
+#endif
